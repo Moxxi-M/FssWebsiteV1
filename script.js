56483476877ded7e0d243be99976dc4c0a794f43
Moxxi-M <desksidelamp@gmail.com>
2023-07-29 14:02:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+// Function to set the color mode preference in local storage
+function setColorModePreference(mode) {
+  localStorage.setItem('color-mode', mode);
+  }
+  
+  // Function to get the color mode preference from local storage
+  function getColorModePreference() {
+  return localStorage.getItem('color-mode');
+  }
+  
+  // Function to set the initial color mode based on the user's preference or system settings
+  function setInitialColorMode() {
+  const savedColorMode = getColorModePreference();
+  if (savedColorMode) {
+  document.body.classList.toggle('dark-mode', savedColorMode === 'dark');
+  } else {
+  // If no preference is saved, check the user's system preference
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  document.body.classList.toggle('dark-mode', prefersDarkMode);
+  setColorModePreference(prefersDarkMode ? 'dark' : 'light');
+  }
+  }
+  
+  // Call the function to set the initial color mode when the page loads
+  setInitialColorMode();
+  
+  // Get the modeSwitch and dynamicText elements from the DOM
+  const modeSwitch = document.getElementById('modeSwitch');
+  const dynamicText = document.getElementById('dynamicText');
+  
+  // Add an event listener to the modeSwitch to toggle dark mode
+  modeSwitch.addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode');
+  updateDynamicText();
+  const currentColorMode = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+  setColorModePreference(currentColorMode);
+  });
+  
+  // Function to toggle the navigation menu visibility
+  function toggleNav() {
+  const navList = document.getElementById("navList");
+  navList.style.display = navList.style.display === "none" ? "inline" : "none";
+  }
+  
+  // Function to toggle dark mode manually (for a separate dark mode button)
+  function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+  const darkModeBtn = document.getElementById("darkModeBtn");
+  darkModeBtn.innerText = body.classList.contains("dark-mode") ? "Light Mode" : "Dark Mode";
+  }
+  
+  // Function to animate the chat button with a chat bubble
+  function animateChat() {
+  const chatButton = document.querySelector('.chat-button');
+  const chatBubble = document.createElement('div');
+  chatBubble.classList.add('chat-bubble');
+  chatButton.appendChild(chatBubble);
+  setTimeout(() => {
+  chatButton.removeChild(chatBubble);
+  }, 800);
+  }
+  
+ 
